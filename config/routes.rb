561aad784eea3dc6f6cc862dd264da6a048a4093
rebeldroid12 <lm.velasquez12@gmail.com>
2015-10-17 16:44:02 -0500

    fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about'  => 'public#about'
   get 'mysheroes' => 'public#mysheroes'
   get  'learn' => 'public#learn'
+  post 'new_shero' => 'public#new_shero'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
