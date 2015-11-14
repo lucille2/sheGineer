ab7675cba92df4ba0d3bd02dbb74f69cb3d4dc65
Hector Rios <hrios@shoplocal.com>
2015-11-14 17:33:27 -0600

    Changed DB source from Postgres to sqlite3
    This commit changes the database store from `Postgres` to `sqlite3`.
    This is being done because we only used fixture data at the Hackathon,
    and we need to deploy this to Heroku.
    
    In order to deploy the application without needing to install any other
    tools, we simplified the data store.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151017171906) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "sheros", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
